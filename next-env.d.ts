9919f565de78b5d6b094009f9a37e55885457593
kr8n3r <jani.kraner@digital.cabinet-office.gov.uk>
2020-06-01 16:22:13 +0100

    Extend SVGImage element to allow src attribute
    By default `src` is not allowed in SVG
    but GOV.UK Frontend use it as a fallback for the crown on IE8
    
    This extend is borrowed from pazmin.

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,12 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+interface IFallbackImage extends React.SVGProps<SVGImageElement> {
+  readonly src: string;
+}
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    readonly image: IFallbackImage;
+  }
+}
