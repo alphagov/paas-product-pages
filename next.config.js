c886ba17d6a6fd517b4e106c3bde9ebfaa5c1b9d
kr8n3r <jani.kraner@digital.cabinet-office.gov.uk>
2020-06-05 10:14:42 +0100

    Use next-mdx-enhanced instead of plain MDX
    We want to pass page frontmatter data to the templates and section
    navigation component so that we don't have to duplicate data.
    
    https://github.com/hashicorp/next-mdx-enhanced allows us to do this.
    
    We've set up a default layout to use if none is specified.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-})
-module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'mdx'],
-  exportTrailingSlash: true,
+const withMdxEnhanced = require('next-mdx-enhanced')
+
+module.exports = withMdxEnhanced({
+  layoutPath: 'layouts/Content/',
+  defaultLayout: true,
+  fileExtensions: ['mdx'],
+  remarkPlugins: [],
+  rehypePlugins: [],
+  extendFrontMatter: {
+    process: (mdxContent, frontMatter) => {},
+    phase: 'prebuild|loader|both',
+  },
+})({
+  exportTrailingSlash: true
 })
