56360f52c4bbbd17fc5e193de4dfccecbd7fcb3b
kr8n3r <jani.kraner@digital.cabinet-office.gov.uk>
2020-06-05 10:13:55 +0100

    Update content layout templates
    Content templates need to be updated with a new way to pase and use
    Frontmatter metadata

--- layouts/PageWithSideNav/index.tsx
+++ layouts/PageWithSideNav/index.tsx
@@ -3,33 +3,34 @@ import Head from 'next/head'
 import SectionNavigation from '@components/SectionNavigation'
 import config from '../../config/config.json'
 
-
-const PageWithSideNav = ({ meta, children }) => (
-  <>
-  <Head>
-    <title>
-      {meta && meta.title ? 
-        (`${meta.title} - ${config.siteName}`)
-        : config.siteName
-      }
-    </title>
-  </Head>
-  <div className="govuk-width-container">
-    <main className="govuk-main-wrapper" id="main-content" role="main">
-      <div className="govuk-grid-row">
-        <div className="govuk-grid-column-one-third">
-          <SectionNavigation />
-        </div>
-        <div className="govuk-grid-column-two-thirds">
-          {meta && meta.title ? (
-            <h1 className="govuk-heading-xl">{meta.title}</h1>
-          ): <></>}
-          {children}
-        </div>
+export default function PageWithSideNav(frontMatter) {
+  return ({ children: content }) => {
+    return (
+      <>
+      <Head>
+        <title>
+          {frontMatter && frontMatter.title ? 
+            (`${frontMatter.title} - ${config.siteName}`)
+            : config.siteName
+          }
+        </title>
+      </Head>
+      <div className="govuk-width-container">
+        <main className="govuk-main-wrapper" id="main-content" role="main">
+          <div className="govuk-grid-row">
+            <div className="govuk-grid-column-one-third">
+              <SectionNavigation />
+            </div>
+            <div className="govuk-grid-column-two-thirds">
+              {frontMatter && frontMatter.title ? (
+                <h1 className="govuk-heading-xl">{frontMatter.title}</h1>
+              ): <></>}
+              {content}
+            </div>
+          </div>
+        </main>
       </div>
-    </main>
-  </div>
-  </>
-)
-
-export default PageWithSideNav
+      </>
+    )
+  }
+}
