cb05896a71011d46a505918b25c2aac2b7bcde80
kr8n3r <jani.kraner@digital.cabinet-office.gov.uk>
2020-06-05 14:23:55 +0100

    Consolidate templates
    Consolidated PageWithSideNav and GenericLayout into a single template
    with a check if page metadata contains a `sectionNav` property.
    If true we render the side navigation.

--- layouts/Content/index.tsx
+++ layouts/Content/index.tsx
@@ -18,9 +18,11 @@ export default function PageWithSideNav(frontMatter) {
       <div className="govuk-width-container">
         <main className="govuk-main-wrapper" id="main-content" role="main">
           <div className="govuk-grid-row">
+            { frontMatter.sectionNav ? ( 
             <div className="govuk-grid-column-one-third">
-              <SectionNavigation />
-            </div>
+              <SectionNavigation section={frontMatter.section} />
+            </div> ) : <></>
+            }
             <div className="govuk-grid-column-two-thirds">
               {frontMatter && frontMatter.title ? (
                 <h1 className="govuk-heading-xl">{frontMatter.title}</h1>
