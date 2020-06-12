408ffeeec31aeb3832a734bae350b1f5f3f9e926
kr8n3r <jani.kraner@digital.cabinet-office.gov.uk>
2020-06-12 15:22:05 +0100

    Mitigate fouc

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,7 @@ import '../styles/application.scss'
 
 export default function ProductPages({ Component, pageProps }) {
   useEffect(() => {
-    document.documentElement.className +=('govuk-template')
     document.documentElement.setAttribute('lang','en')
-    document.body.className += 'js-enabled govuk-template__body';
   }, []);
   return <Component {...pageProps} />
 }
