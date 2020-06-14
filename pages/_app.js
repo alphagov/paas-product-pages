615fb4fbf3e7498b4707dd21a95a6bca04b8fd7f
kr8n3r <jani.kraner@digital.cabinet-office.gov.uk>
2020-06-14 11:15:47 +0100

    Revert  Include govuk-frontend javascript

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,5 @@
-import React, { useEffect } from 'react';
 import '../styles/application.scss'
 
 export default function ProductPages({ Component, pageProps }) {
-  useEffect(() => {
-    document.documentElement.setAttribute('lang','en')
-  }, []);
   return <Component {...pageProps} />
 }
