28658d7d6fe24b4bb665ba34c7f573a8f20f57bd
kr8n3r <jani.kraner@digital.cabinet-office.gov.uk>
2020-06-19 12:42:44 +0100

    Update app.js
    Remove stylesheet import and the script to add 'js-enabled'
    as that is now done in _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,3 @@
-import { useEffect } from 'react'
-import '../styles/application.scss'
-
 export default function ProductPages({ Component, pageProps }) {
-  useEffect(() => {
-    if (typeof window !== 'undefined') { 
-      document.body.className += ' js-enabled';
-    }
-  }, []);
   return <Component {...pageProps} />
 }
