a51aaed1bcb282c5fdefada238b4f285aa2fc3ae
kr8n3r <jani@kraner.dev>
2020-08-21 14:15:34 +0100

    Generate page contents list
    Problem:
    
    DAC audit found that
    
    An underlined hash is displayed with mouse hover of the level-two
    headings on the ‘GOV.UK Platform as a Service’ page. The purpose of the
    hash is unclear as this only appears to direct users’ focus to the
    adjacent heading when selected. Also, they are not accessible to
    standard keyboard commands.
    
    We implemented that feature to allow us to provide deep-links in
    engagement.
    
    Solution:
    
    Instead on providing this functionality on mouse hover only, we now
    generate a page table of contents based on headings' IDs, that are
    anchor links to them. This way we retain the functionality and also
    make it accessible to everyone.
    
    To generate table of content's we're using rehype-toc plugin.
    This generates a TOC on every page, which is undesirable as we don't
    need one on the homepage, the 404 page, the /sign-in page or any page
    that doesn't have headings (like /roadmap).
    
    To remove the TOC from those page, we've created a rehype-plugin. We
    could have created a more comprehensive, own TOC plugin with page
    exclusions, but that would have taken longer.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withMdxEnhanced = require('next-mdx-enhanced')
 const rehypeSlug = require('rehype-slug')
-const rehypeAutolinkHeadings = require('rehype-autolink-headings')
+const toc = require("@jsdevtools/rehype-toc");
+const removeTOC = require("./lib/rehype-removeTOC");
 
 module.exports = withMdxEnhanced({
   layoutPath: 'layouts/Content/',
@@ -9,7 +10,39 @@ module.exports = withMdxEnhanced({
   remarkPlugins: [],
   rehypePlugins: [
     rehypeSlug,
-    rehypeAutolinkHeadings
+    [toc,{
+      headings: ["h2", "h3", "h4"],  
+      cssClasses: {
+        toc: "app-toc",
+        list: "app-toc__list",
+        listItem: "app-toc__list-item",
+        link: "app-toc__link",
+      },
+      customizeTOC: function(toc) {
+        if (toc.type === 'element' && toc.tagName === 'nav') {
+          toc.properties.role = 'navigation';
+        }
+        if (toc.children[0].children.length === 0) {
+          return false
+        } else {
+          toc.children.unshift({
+            type: 'element',
+            tagName: 'h2',
+            children: [
+              {
+                "type": "text",
+                "value": "Contents"
+              }
+            ],
+            properties: {
+              className: 'app-toc__heading'
+            }
+          })
+          return toc
+        }
+      }
+    }],
+    removeTOC
   ],
   extendFrontMatter: {
     process: (mdxContent, frontMatter) => {},
