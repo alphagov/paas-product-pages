7272427cb723087268bbbc6ee12a16a38a96e5bd
kr8n3r <jani@kraner.dev>
2020-12-09 15:45:59 +0000

    Initialise new cookie functions
    If cookie banner is present, initialise its functions
    If on the /cookie spage, initialise its functions
    If user has previously consented to analytics cookies, run GA

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,8 +1,24 @@
 import common from 'govuk-frontend/govuk/common'
 import Button from 'govuk-frontend/govuk/components/button/button'
 import Header from 'govuk-frontend/govuk/components/header/header'
+import Cookies from './cookie-functions'
 
 var nodeListForEach = common.nodeListForEach
+var cookies = new Cookies()
+
+if (cookies.hasConsentForAnalytics()) {
+  cookies.initAnalytics()
+}
+
+var $cookieBanner = document.querySelector('[data-module="cookie-banner"]')
+if ($cookieBanner) {
+  cookies.initCookieBanner($cookieBanner)
+}
+
+var $cookieSettings = document.querySelector('[data-module="cookie-settings"]')
+if ($cookieSettings) {
+  cookies.initCookieSettings($cookieSettings)
+}
 
 var $buttons = document.querySelectorAll('[data-module="govuk-button"]')
 nodeListForEach($buttons, function ($button) {
