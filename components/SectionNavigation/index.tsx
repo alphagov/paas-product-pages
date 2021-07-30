40f72dc2503a9b0c3b8d98d02c84fa8e51cafaf0
kr8n3r <jani@kraner.dev>
2021-07-30 12:36:21 +0100

    Update SectionNavigation component
    Update to reflect the updated data object that gets passed in

--- components/SectionNavigation/index.tsx
+++ components/SectionNavigation/index.tsx
@@ -1,32 +1,16 @@
 import React from 'react'
 import Link from 'next/link'
-import {frontMatter as pages} from '../../pages/*.mdx'
 import { useRouter } from 'next/router'
 
-const SectionNavigation = (section) => {
+const SectionNavigation = (data) => {
+  const items = data.siblings
   // use NextJS to identify the current page for activeNavigation link
   const router = useRouter()
-
-  // because of Staticfile buildpack we need to have index.html pages
-  // but localdev needs to work with / so we format the path for href conditionally
-  // in local dev you'll see 
-  // react-dom.development.js:88 Warning: Prop `href` did not match. error. Ignore.
-  const formatPath = (path) => {
-    return addLeadingSlash(path.replace(/\.mdx$/, ''))
-  }
   
-  const addLeadingSlash = (path) => {
-    return `/${path}`
-  }
-  
-  const pagesInCurrentSection = pages.filter(function(page){
-    return page.section === section.section
-  })
-
   return (
     <nav className="section-navigation" aria-label="Pages in the current section">
       <ol>
-        {pagesInCurrentSection.sort(function (a, b) {
+        {items.sort(function (a, b) {
           if (!a.hasOwnProperty("order") || !b.hasOwnProperty("order")) {
             // property doesn't exist on either object
             return 0;
@@ -34,12 +18,12 @@ const SectionNavigation = (section) => {
           return a.order - b.order;
         })
         .map((page) => (  
-          <li key={page.__resourcePath}
-            className={`section-navigation__item ${formatPath(page.__resourcePath) === router.pathname ? 'section-navigation__item--active' : ''}`}>
-            <Link href={formatPath(page.__resourcePath)}>
+          <li key={page.path}
+            className={`section-navigation__item ${router.asPath === page.path ? 'section-navigation__item--active' : ''}`}>
+            <Link href={page.path}>
               <a 
                 className="govuk-link" 
-                aria-current={formatPath(page.__resourcePath) === router.pathname ? 'page' : null}
+                aria-current={page.path === router.asPath ? 'page' : null}
                 data-track-click="true"
                 data-track-category="Navigation"
                 data-track-action="Side Bar Navigation"
