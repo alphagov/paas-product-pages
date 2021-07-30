24e073a49195c4d6b3432a3caae29dbe138dcfba
kr8n3r <jani@kraner.dev>
2021-07-30 08:46:06 +0100

    Specify custom Typescript types
    In newer versions of Nextjs `next-env.d.ts` get automatically generated and overiides any custom types
    
    Guidance is
    "Instead of editing next-env.d.ts, you can include additional types by adding a new file e.g. additional.d.ts and then referencing it in the include array in your tsconfig.json. "

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,26 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-interface IFallbackImage extends React.SVGProps<SVGImageElement> {
-  readonly src: string;
-}
-
-declare namespace JSX {
-  interface IntrinsicElements {
-    readonly image: IFallbackImage;
-  }
-}
-
-interface FrontMatter {
-  __resourcePath: string;
-  title: string;
-  section: string;
-  navLinkText: string;
-  order: Integer;
-}
-
-declare module "*.mdx" {
-  let MDXComponent: (props: any) => JSX.Element;
-  export default MDXComponent;
-  export const frontMatter: FrontMatter[];
-}
+/// <reference types="next/image-types/global" />
