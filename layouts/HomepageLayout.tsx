755dc7490cbc89c97a11727201ab21bc22511a1f
Jani Kraner <jani.kraner@digital.cabinet-office.gov.uk>
2022-07-20 08:51:12 +0100

    Update homepage
    Remove all sections and add the decomssion notice

--- layouts/HomepageLayout.tsx
+++ layouts/HomepageLayout.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
 import config from '../config/config.json'
-import Masthead from '@components/MastHead'
 
 export default function HomePageLayout({ children }) {
   return (
@@ -9,20 +8,7 @@ export default function HomePageLayout({ children }) {
     <Head>
       <title>{`Host your service in the Cloud - ${config.siteName}`}</title>
     </Head>
-    <div className="app-width-container">
-      <div className="govuk-breadcrumbs app-breadcrumbs app-breadcrumbs--inverse">
-        <ol className="govuk-breadcrumbs__list">
-          <li className="govuk-breadcrumbs__list-item">
-            <a className="govuk-breadcrumbs__link" href="https://www.gov.uk/service-toolkit#gov-uk-services">GOV.UK services</a>
-          </li>
-          <li className="govuk-breadcrumbs__list-item" aria-current="page">GOV.UK PaaS</li>
-        </ol>
-      </div>
-    </div>
     <main id="main-content" role="main">
-      <div className="app-width-container">
-        <Masthead />
-      </div>
       <div className="govuk-width-container">
         <div className="govuk-main-wrapper govuk-main-wrapper--l">
           {children}
