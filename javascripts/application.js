96a106e984fe53fd955b3e50dbf0ebbd0f7f1e2c
Jani Kraner <jani.kraner@digital.cabinet-office.gov.uk>
2023-12-01 07:19:00 +0000

    Remove redundant javascript

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,22 +1,7 @@
 /* eslint-disable no-var, prefer-const, no-new */
 import {
-  Button,
-  Header,
   SkipLink
 } from 'govuk-frontend'
-
-var $buttons = document.querySelectorAll('[data-module="govuk-button"]')
-if ($buttons) {
-  for (var i = 0; i < $buttons.length; i++) {
-    new Button($buttons[i])
-  };
-}
-
-// there is ever only one header per page
-var $header = document.querySelector('[data-module="govuk-header"]')
-if ($header) {
-  new Header($header)
-}
 
 // Find first skip link module to enhance.
 var $skipLink = document.querySelector('[data-module="govuk-skip-link"]')
