bc864fa7116c5e46b3e9619280d9de4235697f5c
Jani Kraner <jani.kraner@digital.cabinet-office.gov.uk>
2024-02-26 08:42:20 +0000

    Remove Jest test setup and config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
-    '@components/(.*)': '<rootDir>/components/$1',
-    '@layouts/(.*)': '<rootDir>/layouts/$1'
-  }
-}
